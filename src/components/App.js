38b013d4acdff6919a783afd285ed5e8ae28d111
SUMAN GHOSH <90588892+sumansingle@users.noreply.github.com>
2023-02-10 20:49:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/App.css';
 import Welcome from './Welcome';
 
-const App = () => {
+function App = () => {
   return (
     <Welcome name={'suman'}/>
   )
